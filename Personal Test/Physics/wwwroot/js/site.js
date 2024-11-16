e78b198ce00ae1b7ed3e43d68d94b9e4607562b6
eperry910 <eperry910@yahoo.com>
2024-11-16 12:41:36 -0500

    Nearly Finished Navigation Elements

--- Personal Test/Physics/wwwroot/js/site.js
+++ Personal Test/Physics/wwwroot/js/site.js
@@ -2,8 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(document).ready(function() {
-    $('.dropdown').click(function() {
-        $(this).next('.dropdown-menu').toggle();
+$(document).ready(function () {
+    $('.dropdown').mouseover(function () {
+        $(this).next('.dropdown-menu').show();
+    });
+
+    $('.dropdown-menu li').mouseover(function () {
+        $(this).find('.sub-dropdown-menu').show();
+    });
+
+    var timeout;
+    $('.dropdown, .dropdown-menu, .sub-dropdown-menu').mouseleave(function () {
+        timeout = setTimeout(function () {
+            $('.dropdown-menu, .sub-dropdown-menu').hide();
+        }, 100);
+    });
+
+    $('.dropdown, .dropdown-menu, .sub-dropdown-menu').mouseenter(function () {
+        clearTimeout(timeout);
     });
 });
